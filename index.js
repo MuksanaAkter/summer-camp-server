b0033267cba19f802a8eb00b13658105200c5f48
muksana akter <muksanaakter3@gmail.com>
2023-06-09 11:35:40 +0600

    delete

--- index.js
+++ index.js
@@ -75,6 +75,13 @@ async function run() {
         res.send(result);
       })
 
+      app.delete('/enrolls/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await enrollCollection.deleteOne(query);
+        res.send(result);
+      })
+
  // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
 
