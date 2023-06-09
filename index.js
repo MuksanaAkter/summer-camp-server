21c5f2c1905b8825d35e5700c75a0ad20e004114
muksana akter <muksanaakter3@gmail.com>
2023-06-09 18:20:38 +0600

    add

--- index.js
+++ index.js
@@ -4,11 +4,32 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
-var jwt = require('jsonwebtoken');
+var jwt = require("jsonwebtoken");
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: true, message: "unauthorized access" });
+  }
+  // bearer token
+  const token = authorization.split(" ")[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res
+        .status(401)
+        .send({ error: true, message: "unauthorized access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.na3b9ip.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -34,14 +55,28 @@ async function run() {
     const instructorCollection = client.db("musicdb").collection("instructor");
     const enrollCollection = client.db("musicdb").collection("enrolls");
 
-
-    app.post('/jwt', (req, res) => {
+    app.post("/jwt", (req, res) => {
       const user = req.body;
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
 
-      res.send({ token })
-    })
-    
+      res.send({ token });
+    });
+
+    // Warning: use verifyJWT before using verifyAdmin
+    // const verifyAdmin = async (req, res, next) => {
+    //   const email = req.decoded.email;
+    //   const query = { email: email };
+    //   const user = await usersCollection.findOne(query);
+    //   if (user?.role !== "admin") {
+    //     return res
+    //       .status(403)
+    //       .send({ error: true, message: "forbidden message" });
+    //   }
+    //   next();
+    // };
+
     ///users
     app.get("/users", async (req, res) => {
       const result = await usersCollection.find().toArray();
@@ -60,6 +95,19 @@ async function run() {
       res.send(result);
     });
 
+    // check admin
+    app.get("/users/admin/:email", verifyJWT, async (req, res) => {
+      const email = req.params.email;
+
+      if (req.decoded.email !== email) {
+        res.send({ admin: false });
+      }
+
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      const result = { admin: user?.role === "admin" };
+      res.send(result);
+    });
     //admin
     app.patch("/users/admin/:id", async (req, res) => {
       const id = req.params.id;
@@ -82,6 +130,11 @@ async function run() {
         .toArray();
       res.send(result);
     });
+    app.post('/classes', async (req, res) => {
+      const newItem = req.body;
+      const result = await menuCollection.insertOne(newItem)
+      res.send(result);
+    })
 
     app.get("/instructors", async (req, res) => {
       const result = await instructorCollection
@@ -92,17 +145,19 @@ async function run() {
     });
 
     // enroll collection apis
-    app.get("/enrolls", async (req, res) => {
+    app.get("/enrolls", verifyJWT, async (req, res) => {
       const email = req.query.email;
 
       if (!email) {
         res.send([]);
       }
 
-      // const decodedEmail = req.decoded.email;
-      // if (email !== decodedEmail) {
-      //   return res.status(403).send({ error: true, message: 'porviden access' })
-      // }
+      const decodedEmail = req.decoded.email;
+      if (email !== decodedEmail) {
+        return res
+          .status(403)
+          .send({ error: true, message: "Forbiden access" });
+      }
 
       const query = { email: email };
       const result = await enrollCollection.find(query).toArray();
