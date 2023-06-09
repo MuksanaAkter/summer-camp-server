f44f4a1251d6d6e4e35dbb8371ee73d9cf7adee2
muksana akter <muksanaakter3@gmail.com>
2023-06-09 13:46:11 +0600

    user

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.na3b9ip.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -18,74 +17,93 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
-
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
-   
 
+    const usersCollection = client.db("musicdb").collection("users");
 
     const database = client.db("musicdb");
     const classCollection = database.collection("classes");
 
-     const instructorCollection =client.db("musicdb").collection("instructor");
-     const enrollCollection = client.db("musicdb").collection("enrolls");
+    const instructorCollection = client.db("musicdb").collection("instructor");
+    const enrollCollection = client.db("musicdb").collection("enrolls");
+    
+    ///users
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await usersCollection.findOne(query);
+
+      if (existingUser) {
+        return res.send({ message: "user already exists" });
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
 
     app.get("/classes", async (req, res) => {
-        const result = await classCollection
-          .find({})
-          // .sort({ createdAt: -1 })
-          .toArray();
-        res.send(result);
-      });
+      const result = await classCollection
+        .find({})
+        // .sort({ createdAt: -1 })
+        .toArray();
+      res.send(result);
+    });
 
-      app.get("/instructors", async (req, res) => {
-        const result = await instructorCollection
-          .find({})
-          // .sort({ createdAt: -1 })
-          .toArray();
-        res.send(result);
-      });
+    app.get("/instructors", async (req, res) => {
+      const result = await instructorCollection
+        .find({})
+        // .sort({ createdAt: -1 })
+        .toArray();
+      res.send(result);
+    });
 
-      // enroll collection apis
-    app.get('/enrolls', async (req, res) => {
-        const email = req.query.email;
-  
-        if (!email) {
-          res.send([]);
-        }
-  
-        // const decodedEmail = req.decoded.email;
-        // if (email !== decodedEmail) {
-        //   return res.status(403).send({ error: true, message: 'porviden access' })
-        // }
-  
-        const query = { email: email };
-        const result = await enrollCollection.find(query).toArray();
-        res.send(result);
-      });
+    // enroll collection apis
+    app.get("/enrolls", async (req, res) => {
+      const email = req.query.email;
 
-      app.post('/enrolls', async (req, res) => {
-        const item = req.body;
-        const result = await enrollCollection.insertOne(item);
-        res.send(result);
-      })
+      if (!email) {
+        res.send([]);
+      }
 
-      app.delete('/enrolls/:id', async (req, res) => {
-        const id = req.params.id;
-        const query = { _id: new ObjectId(id) };
-        const result = await enrollCollection.deleteOne(query);
-        res.send(result);
-      })
+      // const decodedEmail = req.decoded.email;
+      // if (email !== decodedEmail) {
+      //   return res.status(403).send({ error: true, message: 'porviden access' })
+      // }
 
- // Send a ping to confirm a successful connection
+      const query = { email: email };
+      const result = await enrollCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.post("/enrolls", async (req, res) => {
+      const item = req.body;
+      const result = await enrollCollection.insertOne(item);
+      res.send(result);
+    });
+
+    app.delete("/enrolls/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await enrollCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
 
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -93,15 +111,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-
-
-
 app.get("/", (req, res) => {
-    res.send("music is running");
-  });
-  
-  app.listen(port, () => {
-    console.log(`music Server is running on port ${port}`);
-  });
+  res.send("music is running");
+});
+
+app.listen(port, () => {
+  console.log(`music Server is running on port ${port}`);
+});
